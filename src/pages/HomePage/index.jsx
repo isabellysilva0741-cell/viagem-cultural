18e9a02660bbca2fbd25862b484f540026e56cd6
isabelly <isabellysilva0741@gmail.com>
2025-11-19 19:00:38 -0300

    home atualizado

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,20 +4,16 @@ import { TextField } from "../../components/TextField"
 export const HomePage = () => {
     return (
         <main className="min-h-dvh  bg-blue-300">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-                <div className="w-full bg-blue-400 shadow-sm rounded-lg mx-auto">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+                <div className="max-w-full bg-blue-400 shadow-sm mx-auto">
                     <article className="text-center mb-12">
                         <h1 className="text-4xl font-bold text-blue-900">
                             VIAGEM <br />CULTURAL
                         </h1>
                     </article>
-                    <div className="place-content-start" >
+                    <div className="font-medium h-1 " >
                         <h3>RECOMENDAÇÃO</h3>
                     </div>
-                    <h2>DESCUBRA</h2>
-                    <h4>AVALIAÇÃO</h4>
-
-
                 </div>
             </div>
         </main>
